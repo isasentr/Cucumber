364f6f25f9dd267500b8174fdc921388d511706d
isasentr <isasentr@gmail.com>
2022-09-06 20:13:35 +0300

    Apache POI (Excel İşlemleri) 4

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -1,15 +1,21 @@
 package StepDefinitions;
 
+import Utilities.ExcelUtility;
 import Utilities.GenelWD;
 import com.aventstack.extentreports.service.ExtentTestManager;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -22,40 +28,41 @@ public class Hooks {
     }
 
     @After
-    public void after(Scenario scenario)
-    {
+    public void after(Scenario scenario) {
         System.out.println("Senaryo Bitti");
-        System.out.println("scenario sonucu="+ scenario.getStatus());
-        System.out.println("scenario isFailed ?="+ scenario.isFailed());
+        System.out.println("scenario sonucu=" + scenario.getStatus());
+        System.out.println("scenario isFailed ?=" + scenario.isFailed());
 
-        //Excele sonuçları yazdır. path, scenario, browsertipi, zaman parametresi ile
+        //todo Excele sonuçları yazdır. path, scenario, browsertipi, zaman parametresi ile
         //yeni bir excele bütün raporu yazacak. Dosyanın varlığını ve yokluğunu kontrol etmek için googledan bulunuz.
 
-        LocalDateTime date = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yy");
+                        LocalDateTime date = LocalDateTime.now();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yy");
 
-        if (scenario.isFailed()){
-            // klasöre
-            TakesScreenshot screenshot = (TakesScreenshot) GenelWD.getDriver();
-            File ekranDosyasi = screenshot.getScreenshotAs(OutputType.FILE);
+                ExcelUtility.writeExcel("src/test/java/ApachePOI/resource/ScenarioStatus.xlsx",
+                        scenario, GenelWD.threadBrowserName.get(), date.format(formatter));
+
+                if (scenario.isFailed()) {
+                    // klasöre
+                    TakesScreenshot screenshot = (TakesScreenshot) GenelWD.getDriver();
+                    File ekranDosyasi = screenshot.getScreenshotAs(OutputType.FILE);
 
 //Extend Reporta ekleniyor  EXTEND report olmadığında burası kaldırılmalı !!! yoksa browserlar KAPANMAZ
-            ExtentTestManager.getTest().addScreenCaptureFromBase64String(getBase64Screenshot());
+//                    ExtentTestManager.getTest().addScreenCaptureFromBase64String(getBase64Screenshot());
 
-            try {
-                FileUtils.copyFile(ekranDosyasi,
-                        new File("target/FailedScreenShots/"+ scenario.getId()+date.format(formatter)+".png"));
+                    try {
+                        FileUtils.copyFile(ekranDosyasi,
+                                new File("target/FailedScreenShots/" + scenario.getId() + date.format(formatter) + ".png"));
 
-            } catch (IOException e) {
-                e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                // ekran görüntüsü al senaryo hatalı ise
+                GenelWD.quitDriver();
             }
-        }
-        // ekran görüntüsü al senaryo hatalı ise
-        GenelWD.quitDriver();
-    }
-    public String getBase64Screenshot()
-    {
-        return ((TakesScreenshot) GenelWD.getDriver()).getScreenshotAs(OutputType.BASE64);
-    }
+            public String getBase64Screenshot () {
+                return ((TakesScreenshot) GenelWD.getDriver()).getScreenshotAs(OutputType.BASE64);
+            }
 
-}
+        }
--- src/test/java/Utilities/ExcelUtility.java
+++ src/test/java/Utilities/ExcelUtility.java
@@ -1,11 +1,12 @@
 package Utilities;
 
+import io.cucumber.java.Scenario;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.xmlbeans.impl.xb.xsdschema.Public;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,24 +15,24 @@ public class ExcelUtility {
     //   istenilen kolona kadar sütunları okuyup Arraylist formatında geri döndüren fonksiyonu yazınız.
     //   getListData("src/test/java/ApachePOI/resources/ApacheExcel2.xlsx","testCitizen", 2); 0-2
 
-    public static ArrayList<ArrayList<String>> getListData(String path,String sheetName ,int columnCount){
-        ArrayList<ArrayList<String>> tablo=new ArrayList<>();
+    public static ArrayList<ArrayList<String>> getListData(String path, String sheetName, int columnCount) {
+        ArrayList<ArrayList<String>> tablo = new ArrayList<>();
         {
 
-            Workbook workbook=null;
+            Workbook workbook = null;
             try {
                 FileInputStream inputStream = new FileInputStream(path);
-                workbook= WorkbookFactory.create(inputStream);
+                workbook = WorkbookFactory.create(inputStream);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
 
 
-            Sheet sayfa= workbook.getSheet(sheetName);
-            for (int i = 0; i <sayfa.getPhysicalNumberOfRows() ; i++) {
-                ArrayList<String>satirData=new ArrayList<>();
+            Sheet sayfa = workbook.getSheet(sheetName);
+            for (int i = 0; i < sayfa.getPhysicalNumberOfRows(); i++) {
+                ArrayList<String> satirData = new ArrayList<>();
 
-                for (int j = 0; j <columnCount ; j++) {
+                for (int j = 0; j < columnCount; j++) {
                     satirData.add(sayfa.getRow(i).getCell(j).toString());
                 }
                 tablo.add(satirData);
@@ -40,11 +41,86 @@ public class ExcelUtility {
         return tablo;
     }
 
-    //kendi içinde unit test oldu
-    public static void main(String[] args) {
+    // TODO: kendisine verilen    path, scenario, browserTipi, zaman   parametreleri ile
+    // yeni bir excele bütün raporu yazacak. dosyanın varlığını veya yokluğu kontrol etmeyi googdan bulunuz
 
-        ArrayList<ArrayList<String>> tablo= getListData("src/test/java/ApachePOI/resource/ApacheExcel2.xlsx","testCitizen",3);
+    public static void writeExcel(String path, Scenario scenario, String browserName, String time) {
 
-        System.out.println("tablo = " + tablo);
+        File f = new File(path);
+
+        if (!f.exists()) {//!olunca yoksa
+            XSSFWorkbook workbook = new XSSFWorkbook(); //yeni work book oluşturma
+            XSSFSheet sheet = workbook.createSheet("sayfa1");
+            Row newRow = sheet.createRow(0);
+
+            Cell newCell = newRow.createCell(0);
+            newCell.setCellValue(scenario.getName());
+
+            newCell = newRow.createCell(1);
+            newCell.setCellValue(scenario.getStatus().toString());
+
+            newCell = newRow.createCell(2);
+            newCell.setCellValue(browserName);
+
+            newCell = newRow.createCell(3);
+            newCell.setCellValue(time);
+
+            try {
+                FileOutputStream outputStream = new FileOutputStream(path);
+                workbook.write(outputStream);
+                workbook.close();  // hafıza boşaltıldı
+                outputStream.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else
+        {
+            FileInputStream inputStream= null; // okuma modunda açıldı
+            Workbook workbook=null;
+            try {
+                inputStream = new FileInputStream(path);
+                workbook= WorkbookFactory.create(inputStream);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            Sheet sheet=workbook.getSheetAt(0);
+
+            int rowCount = sheet.getPhysicalNumberOfRows(); // 6 satır var  0-5
+            Row newRow = sheet.createRow(rowCount); // en son satırın altına açıldı
+
+            Cell newCell = newRow.createCell(0);
+            newCell.setCellValue(scenario.getName());
+
+            newCell = newRow.createCell(1);
+            newCell.setCellValue(scenario.getStatus().toString());
+
+            newCell = newRow.createCell(2);
+            newCell.setCellValue(browserName);
+
+            newCell = newRow.createCell(3);
+            newCell.setCellValue(time);
+
+            try {
+                inputStream.close();
+                FileOutputStream outputStream=new FileOutputStream(path);
+                workbook.write(outputStream);
+                workbook.close();  // hafıza boşaltıldı
+                outputStream.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
-}
+
+
+            //kendi içinde unit test oldu
+            public static void main (String[]args){
+
+                ArrayList<ArrayList<String>> tablo = getListData("src/test/java/ApachePOI/resource/ApacheExcel2.xlsx", "testCitizen", 3);
+
+                System.out.println("tablo = " + tablo);
+            }
+        }
+
