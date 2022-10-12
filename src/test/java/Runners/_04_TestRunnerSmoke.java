d0fbda7183a418135536168f7bca2dda1ef07550
isasentr <isasentr@gmail.com>
2022-10-12 19:43:54 +0300

    Jenkins Default test çalıştırması için, RunnerClassa plugın ve POM xm… …l e Build eklendi

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -7,7 +7,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         tags = "@SmokeTest", // hangi seneryolarda bu etiket varsa onlar çalıştıralacak
         features = {"src/test/java/FeatureFiles/"}, //buraya feturelerin yolu verilecek.
-        glue = {"StepDefinitions"}// step definitions in klosörü yazılacak.
+        glue = {"StepDefinitions"},// step definitions in klosörü yazılacak.
+        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"} // jenkins için eklendi
 //,plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}  // master report için sonradan ekleniyor
 
 )
